c975fec49c0dd37a7af252e7a415bf19986e1eed
AMADAAN1 <Akshat.Madaan@coxautoinc.com>
2023-12-06 23:24:48 -0600

    Code checkedin

--- Testapplication/Program.cs
+++ Testapplication/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+for (int i = 0; i < 10; i++)
+{
+    try
+    {
+        Console.WriteLine(i);
+    }
+    catch
+    {
+        throw;
+    }
+}
